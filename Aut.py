0b6ec26e25be49a0e58823ac0d74915d18ec9433
Hericlesp <hericlespaulo05@gmail.com>
2024-12-05 09:57:35 -0300

    Update Aut.py

--- Aut.py
+++ Aut.py
@@ -8,21 +8,32 @@ def main(page: ft.Page):
     page.window.maximized = True
     page.window.resizable = False
 
-    login = ft.Column([
+    login = ft.Column([ # aplica configuração a pagina peicipal roxa
         ft.Container(
             bgcolor=ft.colors.PURPLE_400,
             width=page.window_width - 10,
             height= page.window.height - 60,
             border_radius=10,
 
-            content= ft.Column(
+            content= ft.Column([ # aplica configuraçoes a caixa de informações
                 ft.Container(
                     bgcolor=ft.colors.WHITE70,
                     width=400,
                     height=320,
-                    border_radius=10,                    
+                    border_radius=10,  
+
+                    content=ft.Column([
+                        ft.Column([
+                            ft.Text(
+                                #bgcolor=ft.colors.BLACK, a cor aplica a caixa completa
+                                value='Sign-In',
+                                weight='Bold',
+                                size=20
+                            )
+                        ],spacing=20,horizontal_alignment='center')
+                    ])                  
                 )
-            )
+            ], horizontal_alignment='center',alignment='center')
         )
 
     ])
