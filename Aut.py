5b1bc39c81c75827396f290fe7742284ebc3fed5
Hericlesp <hericlespaulo05@gmail.com>
2024-12-05 11:40:06 -0300

    Update Aut.py

--- Aut.py
+++ Aut.py
@@ -10,7 +10,7 @@ def main(page: ft.Page):
 
     login = ft.Column([ # aplica configuração a pagina peicipal roxa
         ft.Container(
-            bgcolor=ft.colors.GREEN_200,
+            bgcolor=ft.colors.PURPLE_400,
             width=page.window_width - 10,
             height= page.window.height - 60,
             border_radius=10,
@@ -24,16 +24,32 @@ def main(page: ft.Page):
 
                     content=ft.Column([
                         ft.Container(
-                            padding=ft.padding.only()
-                            ft.Column([
+                            padding=ft.padding.only(
+                                top=10,
+                                bottom=12
+                            ),
+
+                            content = ft.Column([
                                 ft.Text(
                                     #bgcolor=ft.colors.BLACK, a cor aplica a caixa completa
-                                    value='Sign-In',
+                                    value='Sign-In',color='black',
                                     weight='bold',
                                     size=20
                                 )
-                        ],spacing=20)
-                        )
+                            ])
+                        ),
+                        ft.Column([
+                            ft.TextField(
+                                hint_text='DIGITE SEU EMAIL',
+                                width=300,
+                                height=40,
+                                border_radius=40,
+                                prefix_icon=ft.icons.PERSON,
+                                keyboard_type=ft.KeyboardType.EMAIL
+                            
+                            )
+                        ])
+
                     ],horizontal_alignment='center')                  
                 )
             ], horizontal_alignment='center',alignment='center')
